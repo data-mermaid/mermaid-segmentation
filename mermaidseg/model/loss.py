60186461d50af5392c75a438a4cd1f83de581715
ViktorDo <viktor.domazetoski@hotmail.com>
2025-12-02 01:43:03 +0000

    🚧Added binary loss for concepts

--- mermaidseg/model/loss.py
+++ mermaidseg/model/loss.py
@@ -7,8 +7,11 @@ date: 15-09-2025
 Classes:
     CrossEntropyLoss(torch.nn.CrossEntropyLoss)
 """
+
 from typing import Any
+
 import torch
+
 
 class CrossEntropyLoss(torch.nn.CrossEntropyLoss):
     """
@@ -20,4 +23,19 @@ class CrossEntropyLoss(torch.nn.CrossEntropyLoss):
     """
 
     def __init__(self, ignore_index: int = -1, **kwargs: Any) -> None:
-        super().__init__(ignore_index=ignore_index, **kwargs) 
+        super().__init__(ignore_index=ignore_index, **kwargs)
+
+
+class BCEWithLogitsLoss(torch.nn.BCEWithLogitsLoss):
+    """
+    CrossEntropyLoss is a wrapper of `torch.nn.CrossEntropyLoss` that allows for additional customization.
+    Attributes:
+        ignore_index (int): Specifies a target value that is ignored and does not contribute to the input gradient.
+            This is useful for masking certain values in the target tensor. Defaults to -1.
+        kwargs: Additional keyword arguments that are passed to the base `torch.nn.CrossEntropyLoss` class.
+    """
+
+    def __init__(
+        self, reduction: str = "none", ignore_index: int = -1, **kwargs: Any
+    ) -> None:
+        super().__init__(ignore_index=ignore_index, reduction=reduction, **kwargs)
