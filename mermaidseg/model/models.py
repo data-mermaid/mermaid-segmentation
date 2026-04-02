c7fdce572e4b35c48af9903b1147560377e969ab
Lauren <yeelaurene@gmail.com>
2026-04-01 21:53:21 -0400

    chore: remove Segformer and DINOv2 from main, keep DINOv3 only
    Segformer and LinearDINOv2 are deprioritized and preserved on
    exp_segformer and exp_dinov2 branches respectively. Main now
    only contains LinearDINOv3 and ConceptBottleneckDINOv3.
    
    - Remove Segformer and LinearDINOv2 classes from models.py
    - Remove SegformerForSemanticSegmentation and Dinov2Model imports
    - Delete segformer-mit-b2.yaml, segformer-coralscapes.yaml
    - Delete linear-dinov2-base.yaml, linear-dinov2-base-concept.yaml

--- mermaidseg/model/models.py
+++ mermaidseg/model/models.py
@@ -5,26 +5,29 @@ author: Viktor Domazetoski
 date: 21-10-2025
 
 Classes:
-    Segformer(torch.nn.Module)
-        __init__
-        forward
-        freeze_encoder
-        unfreeze_encoder
-    Dinov2ForSemanticSegmentation(torch.nn.Module)
-        __init__
-        forward
-        freeze_encoder
-        unfreeze_encoder
     LinearClassifier(torch.nn.Module)
         __init__
         forward
+    ConceptHead(torch.nn.Module)
+        __init__
+        forward
+    LinearDINOv3(torch.nn.Module)
+        __init__
+        forward
+        freeze_encoder
+        unfreeze_encoder
+    ConceptBottleneckDINOv3(torch.nn.Module)
+        __init__
+        forward
+        freeze_encoder
+        unfreeze_encoder
 """
 
 import os
 from typing import Any
 
 import torch
-from transformers import AutoModel, Dinov2Model, SegformerForSemanticSegmentation
+from transformers import AutoModel
 from transformers.modeling_outputs import SemanticSegmenterOutput
 
 
@@ -93,7 +96,8 @@ class LinearClassifier(torch.nn.Module):
 
 class ConceptHead(torch.nn.Module):
     """
-    A concept classification head module that performs pixel-wise classification on reshaped embeddings.
+    A concept classification head module that performs pixel-wise classification
+    on reshaped embeddings.
     This module takes input embeddings, reshapes them to a 2D spatial format, and
     applies a 1x1 convolution to perform classification. It's commonly used as a classification
     head for segmentation tasks.
@@ -141,205 +145,19 @@ class ConceptHead(torch.nn.Module):
         return self.concept_classifier(embeddings)
 
 
-class Segformer(torch.nn.Module):
-    """
-    Wrapper around the Segformer segmentation model.
-    This class provides an interface to initialize, train, and use a Segformer model for semantic segmentation tasks.
-    It allows customization of the encoder and the number of output classes, and includes methods to freeze or unfreeze
-    the encoder layers.
-        model (SegformerForSemanticSegmentation): The initialized Segformer model for semantic segmentation.
-    Methods:
-        __init__(encoder_name: str = "nvidia/mit-b2", num_classes: int = 2, **kwargs: Any):
-            Initializes the Segformer model with a specified encoder and number of output classes.
-        forward(x: torch.Tensor) -> torch.Tensor:
-            Performs a forward pass through the model.
-        freeze_encoder():
-            Freezes the encoder layers to prevent them from being updated during training.
-        unfreeze_encoder():
-            Unfreezes the encoder layers to allow them to be updated during training.
-    """
-
-    def __init__(self, encoder_name: str = "nvidia/mit-b2", num_classes: int = 2, **kwargs: Any):
-        """
-        Initializes the semantic segmentation model with a specified encoder and number of classes.
-        Args:
-            encoder_name (str): The name of the HF encoder to use for the model. Defaults to "nvidia/mit-b2".
-            num_classes (int): The number of output classes for semantic segmentation. Defaults to 2.
-            **kwargs (Any): Additional keyword arguments to pass to the model initialization.
-        Attributes:
-            model (SegformerForSemanticSegmentation): The initialized semantic segmentation model.
-        Note:
-            The encoder is frozen by default using the `freeze_encoder` method.
-        """
-        super().__init__()
-        # super(Segformer, self).__init__()
-
-        # self.model = SegformerForSemanticSegmentation.from_pretrained(  # type: ignore
-        #     encoder_name, id2label={i: i for i in range(1, num_classes)}, **kwargs
-        # )
-        self.model = SegformerForSemanticSegmentation.from_pretrained(
-            encoder_name,
-            id2label={i: i for i in range(0, num_classes)},  # do we need a plus one here
-            semantic_loss_ignore_index=0,
-            ignore_mismatched_sizes=True,
-            **kwargs,
-        )
-        # self.freeze_encoder()  # Freeze the backbone - should this be done by default
-
-    def forward(self, x: torch.Tensor, **kwargs: Any) -> torch.Tensor:
-        """
-        Perform a forward pass through the model.
-        Args:
-            x (torch.Tensor): Input tensor to the model.
-        Returns:
-            torch.Tensor: Output tensor after passing through the model.
-        """
-
-        return self.model(x, **kwargs)
-
-    def freeze_encoder(self) -> None:
-        """
-        Freezes the encoder layers of the model by setting the `requires_grad`
-        attribute of their parameters to `False`. This prevents the encoder
-        layers from being updated during training, effectively making them
-        static while allowing other parts of the model to be trained.
-        """
-
-        for param in self.model.segformer.encoder.pixel_level_module.parameters():  # type: ignore
-            param.requires_grad = False
-
-    def unfreeze_encoder(self) -> None:
-        """
-        Unfreezes the encoder layers of the model by setting the `requires_grad`
-        attribute of all parameters in the encoder's feature extraction layers to True.
-        This allows these layers to be trainable during the training process.
-        """
-
-        for param in self.model.segformer.encoder.pixel_level_module.parameters():  # type: ignore
-            param.requires_grad = True
-
-
-# self.model = Dinov2ForSemanticSegmentation.from_pretrained("facebook/dinov2-base",
-#                                                                        id2label={i:i for i in range(0, N_classes)},
-#                                                                        num_labels=N_classes)
-
-
-class LinearDINOv2(torch.nn.Module):
-    """
-    Wrapper around the DINOv2 model for semantic segmentation.
-    This class provides an interface to initialize, train, and use a DINOv2 model for semantic segmentation tasks.
-    It allows customization of the encoder and the number of output classes, and includes methods to freeze or unfreeze
-    the encoder layers.
-        encoder (Dinov2Model): The DINOv2 encoder model.
-        head (LinearClassifier): The classification head for segmentation.
-    Methods:
-        __init__(encoder_name: str = "facebook/dinov2-base", num_classes: int = 2, **kwargs: Any):
-            Initializes the DINOv2 model with a specified encoder and number of output classes.
-        forward(x: torch.Tensor) -> torch.Tensor:
-            Performs a forward pass through the model.
-        freeze_encoder():
-            Freezes the encoder layers to prevent them from being updated during training.
-        unfreeze_encoder():
-            Unfreezes the encoder layers to allow them to be updated during training.
-    """
-
-    def __init__(
-        self,
-        encoder_name: str = "facebook/dinov2-base",
-        num_classes: int = 2,
-        input_size: tuple[int, int] = (518, 518),
-        **kwargs: Any,
-    ):
-        """
-        Initializes the semantic segmentation model with a specified encoder and number of classes.
-        Args:
-            encoder_name (str): The name of the HF encoder to use for the model. Defaults to "facebook/dinov2-base".
-            num_classes (int): The number of output classes for semantic segmentation. Defaults to 2.
-            input_size (tuple[int, int]): The size of the input images (height, width). Defaults to (518, 518).
-            **kwargs (Any): Additional keyword arguments to pass to the model initialization.
-        Attributes:
-            encoder (Dinov2Model): The initialized DINOv2 encoder model.
-            head (LinearClassifier): The classification head for segmentation.
-        Note:
-            The encoder is frozen by default using the `freeze_encoder` method.
-        """
-        super().__init__()
-
-        self.encoder = Dinov2Model.from_pretrained(encoder_name, **kwargs)
-        # Assuming hidden_size=768 for dinov2-base, adjust as needed
-        hidden_size = self.encoder.config.hidden_size
-        patch_size = self.encoder.config.patch_size
-        tokenW = input_size[1] // patch_size
-        tokenH = input_size[0] // patch_size
-
-        self.head = LinearClassifier(
-            hidden_size, tokenW, tokenH, num_classes
-        )  # The tokenW and tokenH are calculated based on input_size and patch_size
-
-        # self.freeze_encoder()  # Freeze the backbone - should this be done by default
-
-    def forward(self, x: torch.Tensor, labels=None, **kwargs: Any) -> torch.Tensor:
-        """
-        Perform a forward pass through the model.
-        Args:
-            x (torch.Tensor): Input tensor to the model.
-            labels (torch.Tensor, optional): Ground truth labels for loss computation.
-        Returns:
-            torch.Tensor: Output tensor after passing through the model.
-        """
-        # use frozen features
-        outputs = self.encoder(x, **kwargs)
-        # get the patch embeddings - so we exclude the CLS token
-        patch_embeddings = outputs.last_hidden_state[:, 1:, :]
-
-        # convert to logits and upsample to the size of the pixel values
-        logits = self.head(patch_embeddings)
-        logits = torch.nn.functional.interpolate(
-            logits, size=x.shape[-2:], mode="bilinear", align_corners=False
-        )
-
-        loss = None
-        # if labels is not None:
-        #     logits = torch.nn.functional.interpolate(
-        #         logits, size=labels.shape[-2:], mode="bilinear", align_corners=False
-        #     )
-        #     loss_fct = torch.nn.CrossEntropyLoss(ignore_index=0)
-        #     loss = loss_fct(logits.squeeze(), labels.squeeze())
-
-        return SemanticSegmenterOutput(loss=loss, logits=logits)
-
-    def freeze_encoder(self) -> None:
-        """
-        Freezes the encoder layers of the model by setting the `requires_grad`
-        attribute of their parameters to `False`. This prevents the encoder
-        layers from being updated during training, effectively making them
-        static while allowing other parts of the model to be trained.
-        """
-        for param in self.encoder.parameters():
-            param.requires_grad = False
-
-    def unfreeze_encoder(self) -> None:
-        """
-        Unfreezes the encoder layers of the model by setting the `requires_grad`
-        attribute of all parameters in the encoder to True.
-        This allows these layers to be trainable during the training process.
-        """
-        for param in self.encoder.parameters():
-            param.requires_grad = True
-
-
 class LinearDINOv3(torch.nn.Module):
     """
     Wrapper around the DINOv3 model for semantic segmentation.
-    This class provides an interface to initialize, train, and use a DINOv3 model for semantic segmentation tasks.
-    It allows customization of the encoder and the number of output classes, and includes methods to freeze or unfreeze
-    the encoder layers.
+    This class provides an interface to initialize, train, and use a DINOv3 model
+    for semantic segmentation tasks. It allows customization of the encoder and the
+    number of output classes, and includes methods to freeze or unfreeze the encoder layers.
     To be able to use the model one needs to authenticate with HuggingFace using the command:
     `huggingface-cli login` and have applied for access to the DINOv3 models.
         encoder (Dinov3Model): The DINOv3 encoder model.
         head (LinearClassifier): The classification head for segmentation.
     Methods:
-        __init__(encoder_name: str = "facebook/dinov3-vits16-pretrain-lvd1689m", num_classes: int = 2, **kwargs: Any):
+        __init__(encoder_name: str = "facebook/dinov3-vits16-pretrain-lvd1689m",
+                 num_classes: int = 2, **kwargs: Any):
             Initializes the DINOv3 model with a specified encoder and number of output classes.
         forward(x: torch.Tensor) -> torch.Tensor:
             Performs a forward pass through the model.
@@ -359,9 +177,12 @@ class LinearDINOv3(torch.nn.Module):
         """
         Initializes the semantic segmentation model with a specified encoder and number of classes.
         Args:
-            encoder_name (str): The name of the HF encoder to use for the model. Defaults to "facebook/dinov3-vits16-pretrain-lvd1689m".
-            num_classes (int): The number of output classes for semantic segmentation. Defaults to 2.
-            input_size (tuple[int, int]): The size of the input images (height, width). Defaults to (512, 512).
+            encoder_name (str): The name of the HF encoder to use for the model.
+                Defaults to "facebook/dinov3-vits16-pretrain-lvd1689m".
+            num_classes (int): The number of output classes for semantic segmentation.
+                Defaults to 2.
+            input_size (tuple[int, int]): The size of the input images (height, width).
+                Defaults to (512, 512).
             **kwargs (Any): Additional keyword arguments to pass to the model initialization.
         Attributes:
             encoder (Dinov2Model): The initialized DINOv2 encoder model.
@@ -437,15 +258,16 @@ class LinearDINOv3(torch.nn.Module):
 class ConceptBottleneckDINOv3(torch.nn.Module):
     """
     Wrapper around the DINOv3 model for semantic segmentation.
-    This class provides an interface to initialize, train, and use a DINOv3 model for semantic segmentation tasks.
-    It allows customization of the encoder and the number of output classes, and includes methods to freeze or unfreeze
-    the encoder layers.
+    This class provides an interface to initialize, train, and use a DINOv3 model
+    for semantic segmentation tasks. It allows customization of the encoder and the
+    number of output classes, and includes methods to freeze or unfreeze the encoder layers.
     To be able to use the model one needs to authenticate with HuggingFace using the command:
     `huggingface-cli login` and have applied for access to the DINOv3 models.
         encoder (Dinov3Model): The DINOv3 encoder model.
         head (LinearClassifier): The classification head for segmentation.
     Methods:
-        __init__(encoder_name: str = "facebook/dinov3-vits16-pretrain-lvd1689m", num_classes: int = 2, **kwargs: Any):
+        __init__(encoder_name: str = "facebook/dinov3-vits16-pretrain-lvd1689m",
+                 num_classes: int = 2, **kwargs: Any):
             Initializes the DINOv3 model with a specified encoder and number of output classes.
         forward(x: torch.Tensor) -> torch.Tensor:
             Performs a forward pass through the model.
@@ -466,9 +288,12 @@ class ConceptBottleneckDINOv3(torch.nn.Module):
         """
         Initializes the semantic segmentation model with a specified encoder and number of classes.
         Args:
-            encoder_name (str): The name of the HF encoder to use for the model. Defaults to "facebook/dinov3-vits16-pretrain-lvd1689m".
-            num_classes (int): The number of output classes for semantic segmentation. Defaults to 2.
-            input_size (tuple[int, int]): The size of the input images (height, width). Defaults to (512, 512).
+            encoder_name (str): The name of the HF encoder to use for the model.
+                Defaults to "facebook/dinov3-vits16-pretrain-lvd1689m".
+            num_classes (int): The number of output classes for semantic segmentation.
+                Defaults to 2.
+            input_size (tuple[int, int]): The size of the input images (height, width).
+                Defaults to (512, 512).
             **kwargs (Any): Additional keyword arguments to pass to the model initialization.
         Attributes:
             encoder (Dinov2Model): The initialized DINOv2 encoder model.
